358976e0691999ff980f4009c9ccc6e12c1a4c22
Vedha-agarwal <82657294+Vedha-agarwal@users.noreply.github.com>
2021-10-30 14:08:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startClassifying() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/aHIOkqYCO/model.json', modelLoaded);
+}
+function modelLoaded(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
